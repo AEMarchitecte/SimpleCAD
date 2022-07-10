bc28e52f361a8eb5c725f1c843650d4c271109a4
AEMarchitecte <baptiste.gobillard@gmail.com>
2022-07-10 15:25:42 +0200

    deplacement-fonctions
    Deplacement des fonctions ini, de global fonction ver ouverture.gd

--- godot_projet/autoload/global_fonction.gd
+++ godot_projet/autoload/global_fonction.gd
@@ -15,20 +15,6 @@ func global_variable_printer():
 
 
 
-func testpersistantfolder():
-	var dir = Directory.new()
-	if dir.dir_exists(simpleCAD_data_dir) == false:
-		print("Dossier ", name_version, " non trouvé...")
-		makepersistantfolder()
-	else:
-		print("Dossier ", name_version, " trouvé...")
-
-func makepersistantfolder():
-	OS.alert(message["first_opening_titre"], message["first_opening_message"])
-	var dir = Directory.new()
-	dir.open(user_data_dir)
-	dir.make_dir(name_version)
-	print("Dossier "+ name_version +" créer avec succés...")
 
 
 
@@ -52,89 +38,6 @@ func makepersistantfolder():
 
 
 
-
-
-
-
-### CONFIG LICENCE FILE ###
-
-func load_encrypt_file():
-	var config = ConfigFile.new()
-	var array = OS.get_unique_id().sha256_buffer()
-	var err = config.load_encrypted(cfg_lic_path, PoolByteArray(array))
-	if err != OK :
-		config.set_value("data", "UID", OS.get_unique_id())
-		config.set_value("data", "nombre_ouverture", 25)
-		config.save_encrypted(cfg_lic_path, PoolByteArray(array))
-		config.clear()
-		print("Fichier créer avec succès = ", cfg_lic_name)
-	else :
-		for data in config.get_sections():
-			var UID = config.get_value(data, "UID")
-			var nombre_ouverture = config.get_value(data, "nombre_ouverture")
-			print("Variables de licence attribuées avec succès...")
-			nombre_ouverture = nombre_ouverture-1
-			print("Nombre ouverture restante = ", nombre_ouverture)
-			if nombre_ouverture <= 0:
-				OS.alert(message["more_opening_message"], message["more_opening_titre"])
-				get_tree().quit()
-				#nombre_ouverture = 0
-			config.set_value("data", "nombre_ouverture", nombre_ouverture)
-			config.save_encrypted(cfg_lic_path, PoolByteArray(array))
-
-
-
-### CONFIG ENVIRONNEMENT FILE ###
-
-func config_environnement_load():
-	var config = ConfigFile.new()
-	var env_cfg = config.load(cfg_env_path)
-	if env_cfg != OK: 
-		config_environnement_create()
-	else: 
-		config_environnement_attribut()
-
-func config_environnement_create():
-	var config = ConfigFile.new()
-	config.set_value("data", "last_user", "Baptiste")
-	config.set_value("data", "WWW", 111)
-	config.save(cfg_env_path)
-	print("Fichier créer avec succès = ", cfg_env_name)
-	
-func config_environnement_attribut():
-	var config = ConfigFile.new()
-	config.load(cfg_env_path)
-	for data in config.get_sections():
-			var last_user = config.get_value(data, "last_user")
-	print("Variables d'environnement attribuées avec succès...")
-
-
-### CONFIG USERS FILE ###
-
-func config_user_load():
-	var config = ConfigFile.new()
-	var use_cfg = config.load(cfg_use_path)
-	if use_cfg != OK:
-		config_user_create()
-	else: 
-		config_user_attribut()
-		
-
-func config_user_create():
-	var config = ConfigFile.new()
-	config.set_value("user0", "identity", defaut_user_identity)
-	config.set_value("user0", "shortcut", defaut_user_shortcuts)
-	config.set_value("user0", "theme", defaut_user_theme)
-	config.set_value("user0", "settings", defaut_user_settings)
-	config.save(cfg_use_path)
-	print("Fichier créer avec succès = ", cfg_use_name)
-
-func config_user_attribut():
-	var config = ConfigFile.new()
-	config.load(cfg_use_path)
-	for data in config.get_sections():
-			last_user = config.get_value(data, "last_user")
-	print("Variables de l'utilisateur attribuées avec succès...")
 
 
 
